c355088ca961d8f185db8e123682a2e56e4b2a3d
huynon201 <ngonnon4@gmail.com>
2024-11-05 18:56:30 +0700

    push sidebar lan 1

---
+++ src/public/admin/js/main.js
@@ -0,0 +1,89 @@
+const sidebar = document.querySelector(".sidebar");
+const sidebarOpenBtn = document.querySelector("#sidebar-open");
+const sidebarCloseBtn = document.querySelector("#sidebar-close");
+const sidebarLockBtn = document.querySelector("#lock-icon");
+const columContent = document.querySelector(".colum-content");
+
+// Function to handle window resize
+const handleResize = () => {
+  if (window.innerWidth < 800) {
+    sidebar.classList.add("close");
+    sidebar.classList.remove("locked");
+    sidebar.classList.remove("hoverable");
+    columContent.classList.remove("locked", "full");
+    columContent.classList.add("closed");
+  } else {
+    sidebar.classList.remove("close");
+    sidebar.classList.add("locked");
+    columContent.classList.remove("closed", "full");
+    columContent.classList.add("locked");
+    if (!sidebar.classList.contains("locked")) {
+      sidebar.classList.add("hoverable");
+      sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
+    } else {
+      sidebar.classList.remove("hoverable");
+      sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
+    }
+  }
+};
+document.body.style.backgroundColor = "blue";
+
+window.addEventListener("resize", handleResize);
+
+//erorr
+
+// Function to toggle the lock state of the sidebar
+const toggleLock = () => {
+  sidebar.classList.toggle("locked");
+  if (!sidebar.classList.contains("locked")) {
+    sidebar.classList.add("hoverable");
+    sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
+    columContent.classList.remove("locked", "full");
+    columContent.classList.add("closed");
+  } else {
+    sidebar.classList.remove("hoverable");
+    sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
+    columContent.classList.remove("closed", "full");
+    columContent.classList.add("locked");
+  }
+};
+
+// Function to hide the sidebar when the mouse leaves
+const hideSidebar = () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+    columContent.classList.remove("locked", "full");
+    columContent.classList.add("closed");
+  }
+};
+
+// Function to show the sidebar when the mouse enters
+const showSidebar = () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+    columContent.classList.remove("closed", "full");
+    columContent.classList.add("locked");
+  }
+};
+
+// Function to toggle the sidebar
+const toggleSidebar = () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    columContent.classList.remove("locked", "full");
+    columContent.classList.add("closed");
+  } else {
+    columContent.classList.remove("closed", "full");
+    columContent.classList.add("locked");
+  }
+};
+
+// Adding event listeners to buttons and sidebar for the corresponding actions
+sidebarLockBtn.addEventListener("click", toggleLock);
+sidebar.addEventListener("mouseleave", hideSidebar);
+sidebar.addEventListener("mouseenter", showSidebar);
+sidebarOpenBtn.addEventListener("click", toggleSidebar);
+sidebarCloseBtn.addEventListener("click", toggleSidebar);
+
+handleResize();
+// main
