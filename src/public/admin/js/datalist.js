8200115cd608f56866e131891873e6fcc0d79a63
huynon201 <ngonnon4@gmail.com>
2024-11-21 01:08:14 +0700

    click sua hien modal

--- src/public/admin/js/datalist.js
+++ src/public/admin/js/datalist.js
@@ -1,37 +1,43 @@
-const input = document.getElementById("category");
-const suggestions = document.getElementById("suggestions");
-const fieldset = document.querySelector("fieldset");
+function initializeAutocomplete(inputId, suggestionsId, fieldsetSelector) {
+  const input = document.getElementById(inputId);
 
-input.onfocus = function () {
-  suggestions.style.display = "block";
-  suggestions.style.width = `${input.offsetWidth}px`; // Căn chỉnh chiều rộng với input
-};
+  const suggestions = document.getElementById(suggestionsId);
+  const fieldset = document.querySelectorAll(fieldsetSelector);
 
-input.oninput = function () {
-  const text = input.value.toUpperCase();
-  let hasResults = false;
-  for (let item of suggestions.children) {
-    if (item.textContent.toUpperCase().indexOf(text) > -1) {
-      item.style.display = "block";
-      hasResults = true;
-    } else {
-      item.style.display = "none";
-    }
-  }
-  suggestions.style.display = hasResults ? "block" : "none";
-};
-
-// Xử lý khi người dùng nhấp vào mục trong danh sách
-for (let item of suggestions.children) {
-  item.onclick = function () {
-    input.value = item.textContent;
-    suggestions.style.display = "none";
+  input.onfocus = function () {
+    suggestions.style.display = "block";
+    suggestions.style.width = `${input.offsetWidth}px`; // Căn chỉnh chiều rộng với input
   };
-}
 
-// Đóng danh sách khi nhấp ra ngoài
-document.addEventListener("click", function (e) {
-  if (!fieldset.contains(e.target)) {
-    suggestions.style.display = "none";
+  input.oninput = function () {
+    const text = input.value.toUpperCase();
+    let hasResults = false;
+    for (let item of suggestions.children) {
+      if (item.textContent.toUpperCase().indexOf(text) > -1) {
+        item.style.display = "block";
+        hasResults = true;
+      } else {
+        item.style.display = "none";
+      }
+    }
+    suggestions.style.display = hasResults ? "block" : "none";
+  };
+
+  // Xử lý khi người dùng nhấp vào mục trong danh sách
+  for (let item of suggestions.children) {
+    item.onclick = function () {
+      input.value = item.textContent;
+      suggestions.style.display = "none";
+    };
   }
-});
+
+  // Đóng danh sách khi nhấp ra ngoài
+  document.addEventListener("click", function (e) {
+    if (!fieldset.contains(e.target)) {
+      suggestions.style.display = "none";
+    }
+  });
+}
+initializeAutocomplete("categoryAdd", "suggestionAdd", "fieldset");
+
+initializeAutocomplete("categoryEdit", "suggestionEdit", "fieldset");
